ad920e5d9fc6808d9488cd26a49d4fb4009a844c
Aadi P <aadipalnitkar96@gmail.com>
2023-01-07 23:46:29 -0600

    Changing terms

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ const App = () => {
 
     return (
         <div className="app">
-            <h1>MovieLand</h1>
+            <h1>MET Gallery Search</h1>
 
             <div className="search">
                 <input
