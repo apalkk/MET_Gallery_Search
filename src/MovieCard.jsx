edaf1dd37cefa848fd8b409d9152a8311a99bb62
AP <108011872+apalkk@users.noreply.github.com>
2023-01-08 01:15:48 -0600

    Update MovieCard.jsx

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 
-const MovieCard = ({ movie: { objectID, objectName, primaryImage, title, period } }) => {
-  return (
-    <div className="movie" key={objectID}>
-      <div>
-        <p>{objectName}</p>
-      </div>
+const MovieCard = ({ movie }) => {
 
-      <div>
-        <img src={primaryImage !== "N/A" ? primaryImage : "https://via.placeholder.com/400"} alt={title} />
-      </div>
+console.log(movie);
 
-      <div>
-        <span>{period}</span>
-        <h3>{title}</h3>
-      </div>
-    </div>
-  );
+return (
+
+<div className="movie" key={movie?.objectID}>
+
+<div>
+
+<p>{movie?.objectName}</p>
+
+</div>
+
+<div>
+
+<img src={movie?.primaryImage !== "N/A" ? movie?.primaryImage : "https://via.placeholder.com/400"} alt={movie?.title} />
+
+</div>
+
+<div>
+
+<span>{movie?.period}</span>
+
+<h3>{movie?.title}</h3>
+
+</div>
+
+</div>
+
+);
+
 }
 
 export default MovieCard;
